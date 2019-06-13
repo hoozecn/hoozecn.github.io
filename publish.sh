119d1d89e4e0a051a4853fefba9f4282cfd3a94f
吴志敏 <wuzhimin@koderover.com>
2019-06-13 14:36:41 +0800

    update

---
+++ publish.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+cd $(dirname $0)
+
+if [ -f token ]; then
+    export CI_TOKEN=$(cat token)
+fi
+
+rm -rf _site && mkdir -p _site
+
+pushd _site
+  rm -rf .git
+  git init
+  
+  git remote add origin https://hoozecn:$CI_TOKEN@github.com/hoozecn/hoozecn.github.io
+  git fetch origin --depth=10
+  git co origin/master -b master
+
+  pushd ..
+    jekyll build
+    rm -f token
+    rm -f publish.sh
+  popd
+
+  git add -A
+  git commit -am 'update'
+  git push origin master:master
+  (cd .. && jekyll clean)
+popd
